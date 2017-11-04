92a062d6c5f750187a36a3ba938cc52c2222b48b
Elena <elenazaharova14@yandex.ru>
2017-11-04 14:57:44 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,17 +13,17 @@ def app(request):
 
 
 def test_add_contact(app):
-     app.login(username="admin", password="secret")
+     app.session.login(username="admin", password="secret")
      app.create_contact(Contact(firstname="11111",middlename= "2222", lastname="33333", nickname="444444", title="5555", company="666", address="777777", home="88888888888",
                            mobile= "9999999999", work="wwwwwwwww", fax="rrrrrrrrr", email="gggggggg", email2="hhhhhhhhhhh", email3="nnnnnnnnnnn",
                            homepage= "ccccccccccc", byear="1234", ayear="3214", address2="fffffffff", phone2="fffffffffff", notes="ffffffff"))
-     app.logout()
+     app.session.logout()
 
 
 def test_add_empty_contact(app):
-     app.login(username="admin", password="secret")
+     app.session.login(username="admin", password="secret")
      app.create_contact(Contact(firstname="",middlename= "", lastname="", nickname="", title="", company="", address="", home="",
                            mobile= "", work="", fax="", email="", email2="", email3="",
                            homepage= "", byear="", ayear="", address2="", phone2="", notes=""))
-     app.logout()
+     app.session.logout()
 
