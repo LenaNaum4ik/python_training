88aa7817c5cb3c47ae5558a4d5cc649218efd749
Elena <elenazaharova14@yandex.ru>
2017-11-04 14:23:41 +0300

    Исправленная фикстура

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 import pytest
 from contact import Contact
-from application_for_contact import Application_for_contact
+from application import Application
 
 
 @pytest.fixture
-def app (request):
-    fixture = Application_for_contact ()
+def app(request):
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
- def test_add_contact(app):
+def test_add_contact(app):
      app.login(username="admin", password="secret")
      app.create_contact(Contact(firstname="11111",middlename= "2222", lastname="33333", nickname="444444", title="5555", company="666", address="777777", home="88888888888",
                            mobile= "9999999999", work="wwwwwwwww", fax="rrrrrrrrr", email="gggggggg", email2="hhhhhhhhhhh", email3="nnnnnnnnnnn",
                            homepage= "ccccccccccc", byear="1234", ayear="3214", address2="fffffffff", phone2="fffffffffff", notes="ffffffff"))
      app.logout()
+
 
 def test_add_empty_contact(app):
      app.login(username="admin", password="secret")
